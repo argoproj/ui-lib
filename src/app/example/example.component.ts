c0728d93aeb4913335f43f4a9389878c5e51fea9
Alexander Matyushentsev <alexander@applatix.com>
2017-03-21 15:28:09 -0700

    Update markdown example

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -46,7 +46,25 @@ export class ExampleComponent  implements OnInit {
         'https://datadog-live.imgix.net/img/Integrations-etcD-340x216.png',
     ];
     icons = [];
-    testMarkdown = '###Applatix is awesome!';
+    testMarkdown = `
+    # README #
+
+This is the Applatix App Store repo. It contains
+
+* The service templates for the published apps.
+* The build scripts, Dockerfiles, and source codes needed to build the images to support the service templates.
+* The README files for each app, containing info on how to use the service templates, and pointers to public github repos that we have used to test them.
+
+## Language Support
+
+* [Golang](golang/README.md)
+* [Java](java/README.md)
+* [Nodejs](nodejs/README.md)
+* [Python](python/README.md)
+
+## Database Support
+
+* [Mysql](mysql/README.md)`;
     tableItems = [];
 
     constructor(private http: Http) {}
